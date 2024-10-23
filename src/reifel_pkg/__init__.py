e600a88bda26b3424116895d11414a3cd4f5f56c
rslgp <rslgp@cin.ufpe.br>
2024-10-23 14:00:19 -0300

    test limit visibility

--- src/reifel_pkg/__init__.py
+++ src/reifel_pkg/__init__.py
@@ -1,3 +1,4 @@
-from .py_reifel import test #exposure public
+# Explicitly declare the public API
+__all__ = ['py_reifel_test', 'py_reifel_fancy_print']
 
-__version__ = "0.0.6"
+from .py_reifel import test as py_reifel_test, fancy_print as py_reifel_fancy_print
